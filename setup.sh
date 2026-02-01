581dad8af78bb0ce9c8da045f9f286c593603fe9
Mr. Olsen Miguel Baez <148258174+BaeztheProg@users.noreply.github.com>
2026-01-31 18:27:48 -0800

    Update setup.sh to move additional Java files
    Added checks to move Book, Movie, and their tests to appropriate directories.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 mkdir -p src/main/java src/test/java
 
-if [ -f Calculator.java ]; then mv Calculator.java src/main/java/ 
+if [ -f Book.java ]; then mv Book.java src/main/java/ 
 fi
 
-if [ -f CalculatorTest.java ]; then mv CalculatorTest.java src/test/java/ 
+if [ -f Movie.java ]; then mv Movie.java src/main/java/ 
+fi
+
+
+if [ -f BookTest.java ]; then mv BookTest.java src/test/java/ 
 fi
 
 cat > build.gradle <<'EOF'
